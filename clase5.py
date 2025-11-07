e0cfb866c93845178b4869b1263e196b2dbf41ad
Cxva97 <cxva97@hotmail.com>
2025-11-07 15:34:00 -0500

    minuto 5:57

---
+++ clase5.py
@@ -0,0 +1,13 @@
+#feedbakc while
+#for
+# variables estructuradas
+
+"""
+Feedback
+condicionales para guiar al codigo en varios camninos en la toma de desiciones
+bucles es utilizar el codigo de una manera repetitiva basada a una condicion"""
+
+numero = 0
+while numero <=10 :
+    print("hola", numero)
+    numero = numero +1
